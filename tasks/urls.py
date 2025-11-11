a40c210b145c4e9cb7753de1a89e35601f5da4c5
MisBoitu <boitumoitshela@gmail.com>
2025-11-12 00:39:16 +0200

    We created task_form.html and tried to make the app responsive

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,12 @@
 # tasks/urls.py
+
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.TaskList.as_view(), name='list'),
     path('create/', views.TaskCreate.as_view(), name='create'),
-    # You'll add update and delete here later
+    path('edit/<int:pk>/', views.TaskUpdate.as_view(), name='edit'), # For full edit
+    path('toggle/<int:pk>/', views.TaskToggleComplete.as_view(), name='toggle_complete'), # For simple button
 ]
+
